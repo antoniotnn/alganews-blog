f382939d403193797ac399da33339160e994f348
Antonio Pires <tonan@outlook.com.br>
2024-07-25 07:10:26 -0300

    Making FeaturedPost responsive

--- components/content.tsx
+++ components/content.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 import styled from "styled-components";
-import { FOOTER_HEIGHT, HEADER_HEIGHT } from "../_constants";
+import {FOOTER_HEIGHT, HEADER_HEIGHT} from "../_constants";
 
 interface ContentProps {
     children: ReactNode;
@@ -15,10 +15,11 @@ export default function Content(props: ContentProps) {
 }
 
 const Wrapper = styled.div`
-  min-height: calc(100vh - ${HEADER_HEIGHT}px - ${FOOTER_HEIGHT}px);
+    min-height: calc(100vh - ${HEADER_HEIGHT}px - ${FOOTER_HEIGHT}px);
 `;
 
 const Container = styled.div`
-  max-width: 848px;
-  margin: auto;
+    max-width: 848px;
+    margin: auto;
+    padding: 16px;
 `;
