bed88d0f478cece4bdfb16a43077180fd78e82ee
Antonio Pires <tonan@outlook.com.br>
2024-07-29 07:41:09 -0300

    Parameters in routes

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,33 @@
+import {ParsedUrlQuery} from "querystring";
+import {GetServerSideProps} from "next";
+import {Post, PostService} from "tnn-sdk";
+
+interface PostProps {
+    post: Post.Detailed
+}
+
+export default function PostPage(props: PostProps) {
+    return <div>{props.post.title}</div>
+}
+
+interface Params extends ParsedUrlQuery {
+    id: string;
+}
+
+export const getServerSideProps: GetServerSideProps<PostProps, Params> = async ({ params }) => {
+
+    if (!params) return { notFound: true };
+
+    const { id } = params;
+    const postId = Number(id);
+
+    if (isNaN(postId)) return { notFound: true };
+
+    const post = await PostService.getExistingPost(postId);
+
+    return {
+        props: {
+            post
+        }
+    }
+}
