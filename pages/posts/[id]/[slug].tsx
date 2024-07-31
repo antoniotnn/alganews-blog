383b879650f14e02bd10761938074042424d832f
Antonio Pires <tonan@outlook.com.br>
2024-07-31 06:26:40 -0300

    Correcting Slug

--- pages/posts/[id]/[slug].tsx
+++ pages/posts/[id]/[slug].tsx
@@ -12,20 +12,27 @@ export default function PostPage(props: PostProps) {
 }
 
 interface Params extends ParsedUrlQuery {
-    pid: string[];
+    id: string;
+    slug: string;
 }
 
-export const getServerSideProps: GetServerSideProps<PostProps, Params> = async ({ params }) => {
+export const getServerSideProps: GetServerSideProps<PostProps, Params> = async ({ params, res }) => {
 
     try {
         if (!params) return { notFound: true };
 
-        const [ id, slug] = params.pid;
+        const {id, slug} = params;
         const postId = Number(id);
 
         if (isNaN(postId)) return { notFound: true };
 
         const post = await PostService.getExistingPost(postId);
+
+        if (slug !== post.slug) {
+            res.statusCode = 301;
+            res.setHeader('Location', `/posts/${post.id}/${post.slug}`);
+            return { props : {} }
+        }
 
         return {
             props: {
