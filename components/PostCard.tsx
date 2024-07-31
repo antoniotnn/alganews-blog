f0effe9b6ace52312e59f9707d63bea8fd946e74
Antonio Pires <tonan@outlook.com.br>
2024-07-31 07:19:41 -0300

    Date formatting with SSR

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import {transparentize} from "polished";
 import Image from "next/image";
 import Link from "next/link";
+import formatPostDate from "../core/utils/formatPostDate";
 
 interface PostCardProps {
     post: Post.Summary;
@@ -18,7 +19,7 @@ export default function PostCard(props: PostCardProps) {
                     <Editor>
                         <EditorImage src={post.editor.avatarUrls.small} width={64} height={64} />
                     </Editor>
-                    <PublishDate>há 3 dias</PublishDate>
+                    <PublishDate>{formatPostDate(post.createdAt)}</PublishDate>
                     <Title>{ post.title }</Title>
                 </Info>
             </Wrapper>
