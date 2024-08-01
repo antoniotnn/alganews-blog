adcc8fd8a380b0415c944dc344d9dd30da512200
Antonio Pires <tonan@outlook.com.br>
2024-08-01 06:46:57 -0300

    Rendering Markdown Posts

---
+++ components/Markdown.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+
+interface MarkdownProps {
+    children: string;
+}
+
+export default function Markdown(props: MarkdownProps) {
+    return (
+        <ReactMarkdown className={'MarkdownRenderer'}>
+            {props.children}
+        </ReactMarkdown>
+    );
+}
--- pages/posts/[id]/[slug].tsx
+++ pages/posts/[id]/[slug].tsx
@@ -4,6 +4,7 @@ import { ParsedUrlQuery } from "querystring";
 import { ResourceNotFoundError } from "tnn-sdk/dist/errors";
 import Head from "next/head";
 import PostHeader from "../../../components/PostHeader";
+import Markdown from "../../../components/Markdown";
 
 interface PostProps extends NextPageProps {
     post?: Post.Detailed;
@@ -28,6 +29,9 @@ export default function PostPage(props: PostProps) {
                         editor={post?.editor}
                         title={post?.title}
                     />
+                    <Markdown>
+                        { post.body }
+                    </Markdown>
                 </>
             )}
         </>
