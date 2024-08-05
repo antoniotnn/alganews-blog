5c22b263e0d4138557c3bf5f3d959a1b6ca070b3
Antonio Pires <tonan@outlook.com.br>
2024-08-05 07:25:45 -0300

    Responsive Reader

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -1,5 +1,5 @@
+import { transparentize } from "polished";
 import { createGlobalStyle } from "styled-components";
-import {transparentize} from "polished";
 
 export default createGlobalStyle`
     * {
@@ -25,57 +25,57 @@ export default createGlobalStyle`
         align-items: center;
         list-style: none;
         gap: 4px;
-    }
 
-    li {
-        a {
-            padding: 0 8px;
-            min-width: 28px;
-            height: 34px;
-            transition: .25s ease;
+        li {
+            a {
+                padding: 0 8px;
+                min-width: 28px;
+                height: 34px;
+                transition: .25s ease;
 
-            display: flex;
-            justify-content: center;
-            align-items: center;
+                display: flex;
+                justify-content: center;
+                align-items: center;
 
-            text-decoration: none;
+                text-decoration: none;
 
-            background-color: ${(p) => p.theme.inactiveElementBackground};
-            color: ${(p) => p.theme.inactiveElementForeground};
-            border-radius: ${(p) => p.theme.borderRadius};
+                background-color: ${(p) => p.theme.inactiveElementBackground};
+                color: ${(p) => p.theme.inactiveElementForeground};
+                border-radius: ${(p) => p.theme.borderRadius};
 
-            font-size: 12px;
-        }
-
-        &.disabled a {
-            cursor: not-allowed;
-            opacity: ${(p) => p.theme.inactiveElementOpacity};
-        }
-
-        &.selected a {
-            background-color: ${(p) => p.theme.primaryBackground};
-            color: ${(p) => p.theme.primaryForeground};
-
-            cursor: default;
-
-            &::before {
-                content: 'Página';
-                display: inline-block;
-                margin-right: 4px;
+                font-size: 12px;
             }
-        }
 
-        &:not(.selected,.disabled) {
-            &:hover,
-            &:focus {
-                a {
-                    transform: translateY(-3px);
-                    box-shadow: 0 3px 6px ${(p) => transparentize(0.9, p.theme.pageForeground)};
+            &.disabled a {
+                cursor: not-allowed;
+                opacity: ${(p) => p.theme.inactiveElementOpacity};
+            }
+
+            &.selected a {
+                background-color: ${(p) => p.theme.primaryBackground};
+                color: ${(p) => p.theme.primaryForeground};
+
+                cursor: default;
+
+                &::before {
+                    content: 'Página';
+                    display: inline-block;
+                    margin-right: 4px;
+                }
+            }
+
+            &:not(.selected,.disabled) {
+                &:hover,
+                &:focus {
+                    a {
+                        transform: translateY(-3px);
+                        box-shadow: 0 3px 6px ${(p) =>
+                                transparentize(0.9, p.theme.pageForeground)};
+                    }
                 }
             }
         }
     }
-
 
     .MarkdownRenderer {
         max-width: 680px;
@@ -128,7 +128,7 @@ export default createGlobalStyle`
             }
         }
 
-        code:not([class^="language"]) {
+        code:not([class^='language']) {
             color: ${(p) => p.theme.pageBackground};
             background-color: ${(p) => p.theme.pageForeground};
             border-radius: ${(p) => p.theme.borderRadius};
@@ -157,5 +157,4 @@ export default createGlobalStyle`
             max-width: 100%;
         }
     }
-
 `;
